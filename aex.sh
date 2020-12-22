28bc7da594e3e3b49fc7f945c454eb680c072f59
Apon77 <khalakuzzamanapon5@gmail.com>
2020-12-23 04:38:49 +0600

    update

--- aex.sh
+++ aex.sh
@@ -5,8 +5,8 @@ send_message_to=571213272               #Give telegram id/group id where message
 
 curl -s -X POST https://api.telegram.org/bot$bot_token/sendMessage -d chat_id=$send_message_to -d text="Build started"
 
-ccache_dir=~/ccache/ 	#Space is not allowed
-max_ccache=28G
+ccache_dir=~/aex/ccache/ 	#Space is not allowed
+max_ccache=100G
 out_base_dir=/mnt/out/
 
 export CCACHE_DIR=$ccache_dir
@@ -21,7 +21,7 @@ export KBUILD_BUILD_USER="k"
 export KBUILD_BUILD_HOST="K"
 #export DEVICE_MAINTAINERS="Khalakuzzaman Apon"
 
-cd /mnt/aex/
+cd ~/aex/
 . build/envsetup.sh
 #lunch superior_tulip-userdebug
 lunch aosp_mido-userdebug
